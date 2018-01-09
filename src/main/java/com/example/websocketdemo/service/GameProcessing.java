83ce8669bfbb897dbf3127c8227375256a459697
DobrokhotovSergey <dobrokhotovsergey@gmail.com>
2018-01-09 22:49:03 +0200

    ready to deploy

--- src/main/java/com/example/websocketdemo/service/GameProcessing.java
+++ src/main/java/com/example/websocketdemo/service/GameProcessing.java
@@ -6,19 +6,27 @@ import com.example.websocketdemo.domain.Move;
 import com.example.websocketdemo.domain.Player;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static com.example.websocketdemo.domain.InitializeCards.doors;
 
 @Slf4j
 public class GameProcessing extends AbstractGameProcessing {
 
+    private static final int NUMBER_DOOR_CARD_ON_START =4;
+
+
+
     public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>(Arrays.asList("Sergey","Anastasia","player1", "player2"));
         GameProcessing game = new GameProcessing();
         game.initializeCard();
-        for(Card card: doors){
-            System.out.println(card);
-        }
+        game.initializePlayers(stringList).forEach(s-> {
+            System.out.println(s);
+        });
 
     }
 
@@ -28,6 +36,29 @@ public class GameProcessing extends AbstractGameProcessing {
         Collections.shuffle(doors);
     }
 
+
+
+    @Override
+    List<Player> initializePlayers(List<String> playerList) {
+        List<Player> players = new ArrayList<>();
+
+        for(int i=0;i<playerList.size();i++){
+            List<Card> cardList = new ArrayList<>();
+            for(int j =0; j < NUMBER_DOOR_CARD_ON_START; j++){
+                cardList.add(doors.get(i+j));
+            }
+            players.add(new Player(playerList.get(i), 1, false, cardList, null));
+        }
+
+
+        return players;
+
+//        В самом начале игроки определяют, кто будет ходить первым.
+//                Каждый игрок получает по 4 карты двери и 4 карты сокровищ.
+//        Перед ходом первого игрока все могут сыграть необходимые им карты (например шмотки, "получи уровень") и игра начинается.
+//        (Проклятия нельзя играть до начала хода первого игрока.)
+
+    }
     @Override
     public void step(Move move) {
         Player player = move.getPlayer();
@@ -42,4 +73,6 @@ public class GameProcessing extends AbstractGameProcessing {
 
 
     }
+
+
 }
