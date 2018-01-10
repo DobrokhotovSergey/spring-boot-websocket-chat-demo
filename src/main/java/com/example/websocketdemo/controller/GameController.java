a3a1f692449b34ba0f9625525b68cb29dcbc1d69
DobrokhotovSergey <dobrokhotovsergey@gmail.com>
2018-01-10 22:32:47 +0200

    ready to deploy

--- src/main/java/com/example/websocketdemo/controller/GameController.java
+++ src/main/java/com/example/websocketdemo/controller/GameController.java
@@ -1,24 +1,26 @@
 package com.example.websocketdemo.controller;
 
+import com.example.websocketdemo.domain.GameField;
 import com.example.websocketdemo.domain.Move;
 import com.example.websocketdemo.domain.Player;
 import com.example.websocketdemo.model.ChatMessage;
+import com.example.websocketdemo.service.GameProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class GameController {
-    private List<Player> players = new ArrayList<Player>(Arrays.asList(
-            new Player("a",0,true,null,null),
-            new Player("b",0,false, null,null)
-            ));
+
+    @Autowired
+    private GameProcessing gameProcessing;
+
+    private Set<String> players = new HashSet<>();
 
 
 //    @MessageMapping("/chat.sendMessage")
@@ -32,25 +34,33 @@ public class GameController {
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        players.add(chatMessage.getSender());
         return chatMessage;
     }
 
-    @MessageMapping("/move")
-    @SendTo("/topic/public")
-    public Move move(@Payload Move move){
-       for(int i=0; i<players.size();i++){
-           if(players.get(i).getName().equals(move.getPlayer().getName())){
-               players.get(i).setMoving(false);
-               if(i+1!=players.size()){
-                   players.get(i+1).setMoving(true);
-               }else {
-                   players.get(0).setMoving(true);
-               }
-
-           }
-       }
+    @MessageMapping("/startGame")
+    @SendTo("/topic/public/start")
+    public GameField start(){
         System.out.println(players);
-        move.setPlayers(players);
-        return move;
+        return gameProcessing.start(new ArrayList<>(players));
     }
+
+//    @MessageMapping("/move")
+//    @SendTo("/topic/public")
+//    public Move move(@Payload Move move){
+//       for(int i=0; i<players.size();i++){
+//           if(players.get(i).getName().equals(move.getPlayer().getName())){
+//               players.get(i).setMoving(false);
+//               if(i+1!=players.size()){
+//                   players.get(i+1).setMoving(true);
+//               }else {
+//                   players.get(0).setMoving(true);
+//               }
+//
+//           }
+//       }
+//        System.out.println(players);
+//        move.setPlayers(players);
+//        return move;
+//    }
 }
